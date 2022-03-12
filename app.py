207dbec86d715573e99cefa54d33d0378455e53a
Pongpanoth Panya <pongpanoth213@gmail.com>
2022-03-12 19:35:25 +0700

    fix CORS problem

--- app.py
+++ app.py
@@ -1,23 +1,33 @@
-from flask import Flask,jsonify, request, render_template
+import os
+
+from flask import Flask,jsonify, request, render_template,make_response
 from flask_restful import Api, Resource
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-api = Api(app)
+CORS(app)
 
-class RecipeSearch(Resource):
-    def get(self):
-        return {"data":"testing result"}
-
-    def post(self):
-        query = request.json['query']
-        score = request.json['score']
-        return "query:" + query + " score: " + score
-
-api.add_resource(RecipeSearch,"/song")
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')  # Change this!
+jwt = JWTManager(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+@cross_origin()
+def create_token():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Wrong username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(user=username,access_token=access_token)
 
 if __name__ == "__main__":
     app.run(debug=True)
